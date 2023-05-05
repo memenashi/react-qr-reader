ef6877ac71bb551fa6a42a9c307dd1865791c2ea
memenashi <shunsuke.vrml@icloud.com>
2023-05-05 19:04:17 +0900

    updating config

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,35 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
 
 import packageJson from './package.json' assert { type: 'json' };
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'named',
-    },
-    {
-      file: packageJson.module,
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve({ browser: true }),
-    commonjs(),
-    typescript(),
-  ],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+      },
+      {
+        file: packageJson.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal({
+        packageJson,
+        include: ['react', 'react-dom'],
+      }),
+      resolve({ browser: true }),
+      commonjs(),
+      typescript(),
+      terser(),
+    ],
+  },
+];
